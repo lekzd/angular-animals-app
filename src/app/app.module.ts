85f34610c4f50ba608ddbd66f1398163fc3d0233
a.korotaev2 <a.korotaev2@tinkoff.ru>
2018-04-04 15:24:35 +0300

    step-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { GuideComponent } from './guide/guide.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    GuideComponent
   ],
   imports: [
     BrowserModule
